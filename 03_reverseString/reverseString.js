a480cbd4ffe1e25ef1efe92d8b4aa807681f0f76
lulu <paraguya.gian@gmail.com>
2023-04-12 21:26:35 -0700

    needed to see solution for task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let result = "";
+    for(let i = 0; i < string.length; i++){
+        if(string.length == 0) return null;
+        
+        
+        let resultString = string;
+        let lastLetter = resultString.slice(-1);
+        resultString = resultString.slice(0, -1);
+        result += lastLetter;
 
+
+
+
+    }
+    return result;
 };
 
 // Do not edit below this line
